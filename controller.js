34543a24f6706457bd1caa4b9ae6a67562612d0f
Griffin Hammer <grifhammer@gmail.com>
2015-11-16 14:22:29 -0500

    Added Display Buttons
    Added display buttons to display one house at a time until all are
    shown.

--- controller.js
+++ controller.js
@@ -1,4 +1,18 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('myController', function($scope){
-	$scope.houses = houses
-	});
+var myApp = angular.module('houseList', []);
+myApp.controller('housesListCtrl', housesListCtrl);
+	
+function housesListCtrl($scope){
+	var numDisplayedHouses = 0;
+	$scope.houses = [];
+	$scope.addHouse = function(){
+		$scope.houses = houses.slice(0,++numDisplayedHouses);
+	}
+	$scope.clearHouses = function(){
+		$scope.houses = [];
+		numDisplayedHouses = 0;
+	}
+
+	$scope.enableAdd = function(){
+		return houses.length > numDisplayedHouses
+	}
+}
